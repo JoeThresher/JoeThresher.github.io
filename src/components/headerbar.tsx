30a7c732d0b6e5078c9ff4f27cc6c91be70b994b
JoeThresher <71785144+JoeThresher@users.noreply.github.com>
2025-02-22 21:58:00 -0500

    Made headerbar sticky

--- src/components/headerbar.tsx
+++ src/components/headerbar.tsx
@@ -3,7 +3,7 @@ import "../index.css";
 export default function Headerbar() {
   return (
     <>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <nav className="bg-white border-gray-200 dark:bg-gray-900 sticky top-0">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
             href="#"
