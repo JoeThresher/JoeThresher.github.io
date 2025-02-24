8911c3517976c278e939d77a30aa19af01cb32b7
JoeThresher <71785144+JoeThresher@users.noreply.github.com>
2025-02-24 16:11:03 -0500

    Cleaned up imports for Pico

--- src/components/headerbar/headerbar.tsx
+++ src/components/headerbar/headerbar.tsx
@@ -3,26 +3,28 @@ import "./headerbar.css";
 export default function Headerbar() {
   return (
     <>
-      <header className="container">
-        <nav>
-          <ul>
-            <li>
-              <strong>Acme Corp</strong>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Services</a>
-            </li>
-            <li>
-              <a href="#">Products</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <div className="headerbar">
+        <header className="container">
+          <nav>
+            <ul>
+              <li>
+                <strong>Acme Corp</strong>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Products</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>
     </>
   );
 }
