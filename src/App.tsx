9348e9245f078d814fb758f58f43ca98ff8cce9c
JoeThresher <71785144+JoeThresher@users.noreply.github.com>
2025-02-24 13:48:01 -0500

    Moved components into their own folders

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./index.css";
-import Hero from "./components/hero.tsx";
-import Headerbar from "./components/headerbar.tsx";
+import Hero from "./components/hero/hero.tsx";
+import Headerbar from "./components/headerbar/headerbar.tsx";
 
 export default function App() {
   const [count, setCount] = useState(0);
