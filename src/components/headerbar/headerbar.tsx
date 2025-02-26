f25792c6b67c093c43a9ab93ffe2bee695bc2d92
JoeThresher <71785144+JoeThresher@users.noreply.github.com>
2025-02-26 13:26:09 -0500

    Successfully changed framework from Pico to Bootstrap

--- src/components/headerbar/headerbar.tsx
+++ src/components/headerbar/headerbar.tsx
@@ -3,28 +3,61 @@ import "./headerbar.css";
 export default function Headerbar() {
   return (
     <>
-      <div className="headerbar">
-        <header className="container">
-          <nav>
-            <ul>
-              <li>
-                <strong>Acme Corp</strong>
+      <nav className="navbar navbar-expand-md navbar-light">
+        <div className="container-xxl">
+          <a href="#intro" className="navbar-brand">
+            <span className="fw-bold text-secondary">
+              <i className="bi bi-book-half" />
+              Net Ninja Pro - The Book
+            </span>
+          </a>
+          {/* Toggle button for mobile nav */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#main-nav"
+            aria-controls="main-nav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          {/* navbar links */}
+          <div
+            className="collapse navbar-collapse justify-content-end align-center"
+            id="main-nav"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#topics">
+                  About The Book
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#reviews">
+                  Reviews
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#contact">
+                  Get in Touch
+                </a>
+              </li>
+              <li className="nav-item d-md-none">
+                <a className="nav-link" href="#pricing">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item ms-2 d-none d-md-inline">
+                <a className="btn btn-secondary" href="#pricing">
+                  buy now
+                </a>
               </li>
             </ul>
-            <ul>
-              <li>
-                <a href="#">About</a>
-              </li>
-              <li>
-                <a href="#">Services</a>
-              </li>
-              <li>
-                <a href="#">Products</a>
-              </li>
-            </ul>
-          </nav>
-        </header>
-      </div>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
